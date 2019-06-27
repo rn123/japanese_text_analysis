1935a02df73c8fc41666a49acfe96649345b2f47
rn123 <ray.nowell@gmail.com>
2019-06-27 10:54:32 -1000

    initial creation of japanese text analysis experiments

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a set of experiments to examine word usage trends in Japanese texts.',
+    author='Ray Nowell',
+    license='',
+)
